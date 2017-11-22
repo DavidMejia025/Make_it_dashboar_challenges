a181aebdf863e808f4d48f318c8e626733b4ffa8
david <davidndy@gmail.com>
2017-11-22 18:03:13 -0500

    JS min max challenge

---
+++ JS/max_min.js
@@ -0,0 +1,9 @@
+
+Hola Maker,
+
+Tu misión en este reto es definir dos funciones, una llamada maximo y otra minimo, que reciben un número variable de argumentos (números) y retornan el máximo y el mínimo respectivamente.
+
+
+maximo(5, 10, 20, 50, 32) #=> 50
+
+minimo(3, 44, 2, 8) #=> 2
