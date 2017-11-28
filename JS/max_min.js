ffccc0389f31468795a8e47bc88b28045faffd30
OEM Configuration (temporary user) <davidndy@gmail.com>
2017-11-28 11:06:12 -0500

    Javascript min max challenge from  make it real dashboard DONE

--- JS/max_min.js
+++ JS/max_min.js
@@ -1,9 +1,31 @@
 
-Hola Maker,
+// Hola Maker,
 
-Tu misión en este reto es definir dos funciones, una llamada maximo y otra minimo, que reciben un número variable de argumentos (números) y retornan el máximo y el mínimo respectivamente.
+// Tu misión en este reto es definir dos funciones, una llamada maximo y otra minimo, que reciben un número 
+// variable de argumentos (números) y retornan el máximo y el mínimo respectivamente.
 
 
-maximo(5, 10, 20, 50, 32) #=> 50
+function maximo(numbers){
+	var arg =[]
+	for ( var i=0; i<arguments.length;i++){
+		arg.push(arguments[i])
+	}
+	var max = arg.sort(function(a, b){return a-b});
+	return max = max[max.length-1]
+	console.log(arg)
+}
 
-minimo(3, 44, 2, 8) #=> 2
+ function minimo(numbers){
+	var arg =[]
+	for ( var i=0; i<arguments.length;i++){
+		arg.push(arguments[i])
+	}
+	var min = arg.sort(function(a, b){return a-b});
+	return min = min[0]
+}
+
+n = maximo(5, 10, 20, 50, 32)
+m = minimo(3, 44, 2, 8)
+console.log( n )
+console.log( m )
+ 
