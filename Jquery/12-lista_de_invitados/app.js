724accfc963eb2c650763927e6729086a76cc22e
OEM Configuration (temporary user) <davidndy@gmail.com>
2017-12-21 18:43:36 -0500

    FInishing the invites list challenge. It is working on the nav buy doesnt pass the test

--- Jquery/12-lista_de_invitados/app.js
+++ Jquery/12-lista_de_invitados/app.js
@@ -1,22 +1,42 @@
-var data_name = "Hola";
-var data_surname = "";
-var data_mp = "";
-var data_cb = false;
+$( document ).ready(function() {
+  $(".form-horizontal").on("submit", function(){
+    event.preventDefault()
+    var first_name = $("#first-name").val()
+    var last_name = $("#last-name").val()
+    var mobile_number = $("#mobile-number").val()
+    var confirmed = $("#confirmed").is(':checked')
+    $("tbody").append(`<tr> <td>${first_name}</td> <td>${last_name}</td> <td class="text-center">${mobile_number}</td> <td class="text-center">${confirmed}</td> </tr>`)
+    $("#first-name").val("")
+    $("#last-name").val("")
+    $("#mobile-number").val("")
+    $("#confirmed").prop("checked", false)
+    $("#new-guest").modal('toggle');
+
+  })
+});
 
 
 
-//$('.modal-footer .btn.btn-primary').on('click',function(e){
-$('form').submit(function() {
-	data_name= $("#first-name").val()
-	data_surname= $("#last-name").val()
-	data_mp= $("#mobile-number").val()
-	data_cb= $("#confirmed").is(":checked")
-	$('.table').append(`<tr><td>${data_name}</td><td>${data_surname}</td><td>${data_mp}</td><td>${data_cb}</td></tr>`);
-
-})
 
 
-// 	e.preventDefault(); 
+
+
+
+
+
+// var data_name = "Hola";
+// var data_surname = "";
+// var data_mp = "";
+// var data_cb = false;
+
+
+
+
+
+
+
+// //$('.modal-footer .btn.btn-primary').on('click',function(e){
+// $('form').submit(function() {
 // 	data_name= $("#first-name").val()
 // 	data_surname= $("#last-name").val()
 // 	data_mp= $("#mobile-number").val()
@@ -24,3 +44,13 @@ $('form').submit(function() {
 // 	$('.table').append(`<tr><td>${data_name}</td><td>${data_surname}</td><td>${data_mp}</td><td>${data_cb}</td></tr>`);
 
 // })
+
+
+// // 	e.preventDefault(); 
+// // 	data_name= $("#first-name").val()
+// // 	data_surname= $("#last-name").val()
+// // 	data_mp= $("#mobile-number").val()
+// // 	data_cb= $("#confirmed").is(":checked")
+// // 	$('.table').append(`<tr><td>${data_name}</td><td>${data_surname}</td><td>${data_mp}</td><td>${data_cb}</td></tr>`);
+
+// // })
