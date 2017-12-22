acc7557637d133255b6be85e8dd82d4b964d4297
OEM Configuration (temporary user) <davidndy@gmail.com>
2017-12-21 21:01:28 -0500

    3nd try country list

--- Jquery/mostrarPaises/app.js
+++ Jquery/mostrarPaises/app.js
@@ -8,6 +8,11 @@
 $(".wrapper button").on("click", function(){
 	$(".wrapper").hide()
 	//$("body").append("<table><thead><tr><th> 'Flag' </th> <th> 'Code'</th><th> 'Name'</th></tr></thead> <tbody></tbody></table>"
+  	
+	$("body").append("<table></table>")
+
+	$("table").append("<tr><th> Flag </th><th> Code </th><th> name </th> </tr>")
+	$("table").append("<tbody> </tbody>")
   	$("table").css("display","block")
 	for ( var i=0; i<4; i++){
 		$("tbody").append(`<tr> <td><img src="${countries[i].flag_url}" alt="" /> </td> <td> ${countries[i].code} </td> <td> ${countries[i].name} </td> </tr>`)
