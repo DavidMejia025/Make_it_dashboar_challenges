63613c3b0767cc4544e0b6026e7175d379f357dc
OEM Configuration (temporary user) <davidndy@gmail.com>
2017-12-26 17:40:51 -0500

    Finally the guest list is working. It was necessary to use handlebars in order to handle better the html edition from a js file.

--- Jquery/12-lista_de_invitados/app.js
+++ Jquery/12-lista_de_invitados/app.js
@@ -1,19 +1,37 @@
-$( document ).ready(function() {
-  $(".form-horizontal").on("submit", function(){
-    event.preventDefault()
-    var first_name = $("#first-name").val()
-    var last_name = $("#last-name").val()
-    var mobile_number = $("#mobile-number").val()
-    var confirmed = $("#confirmed").is(':checked')
-    $(".table").append(`<tr> <td>${first_name}</td> <td>${last_name}</td> <td class="text-center">${mobile_number}</td> <td class="text-center">${confirmed}</td> </tr>`)
-    $("#first-name").val("")
-    $("#last-name").val("")
-    $("#mobile-number").val("")
-    $("#confirmed").prop("checked", false)
-    $("#new-guest").modal('toggle');
+var template = Handlebars.compile($('#guest-template').html());
 
-  })
+$('form').on('submit', function(e) {
+  e.preventDefault();
+
+  var guest = { 
+    firstName: $('#first-name').val(), 
+    lastName: $('#last-name').val(), 
+    mobileNumber: $('#mobile-number').val(),
+    confirmed: $('#confirmed').is(':checked')
+  };
+
+  $('table tbody').append(template(guest));
+  $('#new-guest').modal('hide');
+  $(this)[0].reset();
 });
+
+// $( document ).ready(function() {
+//   $(".form-horizontal").on("submit", function(){
+//     event.preventDefault()
+//     var first_name = $("#first-name").val()
+//     var last_name = $("#last-name").val()
+//     var mobile_number = $("#mobile-number").val()
+//     var confirmed = $("#confirmed").is(':checked')
+//     $(".table").append(`<tr> <td>${first_name}</td> <td>${last_name}</td> <td class="text-center">${mobile_number}</td> <td class="text-center">'<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>'</td> </tr>`)
+//     //$('.table tbody').append('<tr>' + '<td>' + nombre + '</td><td>' + apellido + '</td><td>' + celular + '</td><td>' + '<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>'  + '</td></tr>')
+//     $("#first-name").val("")
+//     $("#last-name").val("")
+//     $("#mobile-number").val("")
+//     $("#confirmed").prop("checked", false)
+//     $("#new-guest").modal('toggle');
+
+//   })
+// });
 
 
 
