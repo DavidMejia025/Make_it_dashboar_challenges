29c95f8730ab64c074b69a8d82cb6413030cc38e
OEM Configuration (temporary user) <davidndy@gmail.com>
2017-12-26 20:14:42 -0500

    Countries list donde. Performed with handlerbars

--- Jquery/mostrarPaises/app.js
+++ Jquery/mostrarPaises/app.js
@@ -7,16 +7,24 @@
 
 $(".wrapper button").on("click", function(){
 	$(".wrapper button").hide()
-	//$("body").append("<table><thead><tr><th> 'Flag' </th> <th> 'Code'</th><th> 'Name'</th></tr></thead> <tbody></tbody></table>"
-  	
-	$(".wrapper").append("<table></table>")
+	var source = $('#country-template').html()
+	var template = Handlebars.compile(source)
 
-	$("table").append("<tr><th> Flag </th><th> Code </th><th> name </th> </tr>")
-	$("table").append("<tbody> </tbody>")
-  	$("table").css("display","block")
-	for ( var i=0; i<4; i++){
-		$("tbody").append(`<tr> <td><img src="${countries[i].flag_url}" alt="" /> </td> <td> ${countries[i].code} </td> <td> ${countries[i].name} </td> </tr>`)
-	}
-
+	var html =template({"paises":countries})
+	$(".wrapper").html(html)
 })
+// $(".wrapper button").on("click", function(){
+// 	$(".wrapper button").hide()
+// 	//$("body").append("<table><thead><tr><th> 'Flag' </th> <th> 'Code'</th><th> 'Name'</th></tr></thead> <tbody></tbody></table>"
+  	
+// 	$(".wrapper").append("<table></table>")
+
+// 	$("table").append("<tr><th> Flag </th><th> Code </th><th> name </th> </tr>")
+// 	$("table").append("<tbody> </tbody>")
+//   	$("table").css("display","block")
+// 	for ( var i=0; i<4; i++){
+// 		$("tbody").append(`<tr> <td><img src="${countries[i].flag_url}" alt="" /> </td> <td> ${countries[i].code} </td> <td> ${countries[i].name} </td> </tr>`)
+// 	}
+
+// })
 
